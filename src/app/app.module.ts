5a94c818d9f608bbeb4fdd7006efdefd6feb3196
Vitor Oliveira <vitoroliveira.ads@gmail.com>
2018-03-26 21:06:45 -0300

    Tchoco - Adicionado NavBar e arquivos de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { FuncionarioComponent } from './funcionario/funcionario.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuncionarioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
